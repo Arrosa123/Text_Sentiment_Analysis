93429f39a9583844bad65469883708f7161c7a49
unknown <davekiran@gmail.com>
2022-06-07 19:28:23 -0700

    trying to make tables

---
+++ Machine_Learning.py
@@ -0,0 +1,238 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import sys
+
+import h2o
+import pandas as pd
+import numpy as np
+from scipy.stats import pearsonr #correlation
+from sklearn.feature_extraction.text import TfidfVectorizer
+import H2OGBMClassifier as hgb
+from sklearn.metrics import roc_auc_score
+import sys
+from collections import defaultdict
+import operator
+#import sys
+
+#reload(sys)  # Reload does the trick!
+#sys.setdefaultencoding('utf-8')
+
+#Dataset provided 
+#file = './sentiment_m140_.csv'
+#twitter_df = pd.read_csv(file)
+#twitter_df = twitter_df.rename(columns={"target": "polarity", "ids": "id", "flag" : "query"})
+#twitter_df["new_date"] = twitter_df["date"]
+
+# 
+# REPLACE WITH CODE TO ACCESS FROM S3
+#
+#Dataset Create
+file = './static/resources/sentiment_analysis_10k.csv' 
+
+def train_model(file):
+    twitter_df = pd.read_csv(file,encoding = "ISO-8859-1")
+    #
+    #
+    #
+
+
+
+    print (twitter_df.shape)
+    #Update the target to 0 or 1 -- came in as 0 or 4 
+    twitter_df.loc[twitter_df["polarity"] == 4,"polarity"] = 1
+    print (" average target is %f "  % np.mean(twitter_df["polarity"]))
+
+
+
+    # Split up positive and negative tweets into two dataframes
+    positive_tweets_df = twitter_df.loc[twitter_df["polarity"] ==1]
+    negative_tweets_df = twitter_df.loc[twitter_df["polarity"] ==0]
+
+    train_pos = positive_tweets_df[:int(len(positive_tweets_df)*0.8)]
+    test_pos = positive_tweets_df[int(len(positive_tweets_df)*0.8):]
+    train_neg = negative_tweets_df[:int(len(positive_tweets_df)*0.8)]
+    test_neg = negative_tweets_df[int(len(positive_tweets_df)*0.8):]
+
+    # combine positive and negative labels
+    train_X = pd.concat([train_pos ,train_neg]) 
+    test_X = pd.concat([test_pos ,test_neg])
+
+    # create our target arrays
+    train_y = train_X["polarity"]
+    test_y = test_X["polarity"]
+
+
+    # Remove the columns we're not using as features
+    train_X.drop(["polarity"],inplace=True, axis=1)
+    train_X.drop(["id"],inplace=True, axis=1)
+    train_X.drop(["date"],inplace=True, axis=1)
+    train_X.drop(["query"],inplace=True, axis=1)
+    train_X.drop(["user"],inplace=True, axis=1)
+    train_X.drop(["new_date"],inplace=True, axis=1)
+
+    test_X.drop(["polarity"],inplace=True, axis=1)
+    test_X.drop(["id"],inplace=True, axis=1)
+    test_X.drop(["date"],inplace=True, axis=1)
+    test_X.drop(["query"],inplace=True, axis=1)
+    test_X.drop(["user"],inplace=True, axis=1)
+    test_X.drop(["new_date"],inplace=True, axis=1)
+
+
+    #tf-idf model
+    tfv=TfidfVectorizer(min_df=0, max_features=3000, strip_accents='unicode',lowercase =True,
+                                analyzer='word', token_pattern=r'\w{3,}', ngram_range=(1,1),
+                                use_idf=True,smooth_idf=True, sublinear_tf=True, stop_words = "english")   
+    #h2o gbm model
+    model=hgb.H2OGBMClassifier (ntrees=100,
+                                learn_rate=0.1,
+                                distribution="bernoulli",
+                                col_sample_rate=1.0,
+                                col_sample_rate_per_tree =0.5,
+                                nthread=15,
+                                sample_rate=0.9,
+                                stopping_metric="logloss",
+                                nbins=255,
+                                min_rows=1,
+                                ram="20G",
+                                max_depth=4,
+                                seed=1)
+
+    #apply tf idf
+    data=tfv.fit_transform(train_X["text"].to_numpy())
+    data=data.toarray()
+    print (data.shape) 
+
+
+    test_data = tfv.fit_transform(test_X["text"].to_numpy())
+    test_data = test_data.toarray()
+    print(test_data.shape)
+
+
+
+    #fit model
+    model.fit(data,np.array(train_y)) # feed target
+
+
+
+    #make predictions)probabilities) on tweets
+    preds=model.predict_proba(test_data)[:,1]
+
+
+    print ("training auc is %f" %roc_auc_score(test_y,preds) )
+
+
+
+    from sklearn.ensemble import GradientBoostingClassifier
+
+    # Create a classifier object
+    learning_rates = [0.1]
+    for learning_rate in learning_rates:
+        classifier = GradientBoostingClassifier(n_estimators=100,
+                                                learning_rate=learning_rate,
+                                                max_features=20,
+                                                max_depth=4)
+
+        # Fit the model
+        classifier.fit(data, train_y)
+        print("Learning rate: ", learning_rate)
+
+        # Score the model
+        print("Accuracy score (training): {0:.3f}".format(
+            classifier.score(
+                data,
+                train_y)))
+        print("Accuracy score (validation): {0:.3f}".format(
+            classifier.score(
+                test_data,
+                test_y)))
+
+
+
+    GBS_preds=classifier.predict_proba(test_data)[:,1]
+
+
+
+    # Make Prediction
+    gbs_predictions = classifier.predict(test_data)
+
+
+
+    test_X["target"] = test_y
+    test_X["GBS_Prediction"] = gbs_predictions
+    test_X["GBS_preds"] = GBS_preds
+
+
+
+    test_X.to_csv("test_results.csv")
+
+
+
+    from sklearn.metrics import confusion_matrix
+    from sklearn.metrics import accuracy_score
+    from sklearn.metrics import classification_report
+    # Calculating the accuracy score
+    acc_score = accuracy_score(test_y, gbs_predictions)
+    print(f"Accuracy Score : {acc_score}")
+
+
+
+    # Generate the confusion matrix
+    cm = confusion_matrix(test_y, gbs_predictions)
+    cm_df = pd.DataFrame(
+        cm, index=["Actual 0", "Actual 1"],
+        columns=["Predicted 0", "Predicted 1"]
+    )
+
+
+    from sklearn.ensemble import RandomForestClassifier
+    rfc = RandomForestClassifier(n_estimators=100, max_depth=4, max_features =50)
+
+
+
+    # Fit the model
+    rfc.fit(data, train_y)
+
+
+    RFC_preds=rfc.predict_proba(test_data)[:,1]
+
+
+
+    # Make Prediction
+    RFC_predictions = rfc.predict(test_data)
+
+
+    test_X["target"] = test_y
+    test_X["RFC_Prediction"] = RFC_predictions
+    test_X["RFC_preds"] = RFC_preds
+
+
+
+    from sklearn.metrics import confusion_matrix
+    from sklearn.metrics import accuracy_score
+    from sklearn.metrics import classification_report
+    # Calculating the accuracy score
+    acc_score = accuracy_score(test_y, RFC_predictions)
+    print(f"Accuracy Score : {acc_score}")
+
+
+
+    # Generate the confusion matrix
+    cm = confusion_matrix(test_y, RFC_predictions)
+    cm_df = pd.DataFrame(
+        cm, index=["Actual 0", "Actual 1"],
+        columns=["Predicted 0", "Predicted 1"]
+    )
+
+
+    test_X.to_csv("test_results.csv")
+
+    return(cm_df)
+
+def eval_text(text):
+    body = {}
+    body["eval_title"] = "Starting Title"
+    body["eval_paragraph"] = "A Paragraph to start with"
+    print(body)
+    return(body)  
---
+++ twitter_api_stream.py
@@ -0,0 +1,160 @@
+import requests
+import os
+import json
+import pandas as pd
+from flask import session
+
+
+# To set your enviornment variables in your terminal run the following line:
+# export 'BEARER_TOKEN'='<your_bearer_token>'
+with open('myconfig.json','r') as fh:
+    config = json.load(fh)
+os.environ["BEARER_TOKEN"] = config["BEARER_TOKEN"]
+
+bearer_token = os.environ.get("BEARER_TOKEN")
+print(bearer_token)
+
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
+
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    r.headers["User-Agent"] = "v2FilteredStreamPython"
+    return r
+
+def create_rules(hashtag_data):
+        new_rules = '{"rules" : ['
+        counter = 0
+        for hashtag in hashtag_data['tw_trends']:
+            counter = counter + 1
+            if counter == 1:
+                new_rules = new_rules + '{"value": "' + hashtag['hashtag'] + ' -is:retweet lang:en -has:links -has:media", "tag": "' + hashtag['hashtag'] + '"}'
+            else:
+                new_rules = new_rules + ',{"value": "' + hashtag['hashtag'] + ' -is:retweet lang:en -has:links -has:media", "tag": "' + hashtag['hashtag'] + '"}'
+        new_rules = new_rules + ']}'        
+        print(new_rules)
+        return(new_rules)
+
+def get_rules():
+    response = requests.get(
+        "https://api.twitter.com/2/tweets/search/stream/rules", auth=bearer_oauth
+    )
+    if response.status_code != 200:
+        raise Exception(
+            "Cannot get rules (HTTP {}): {}".format(response.status_code, response.text)
+        )
+    print('get_rules() response:')    
+    print(json.dumps(response.json()))
+    return response.json()
+
+
+def delete_all_rules(rules):
+    if rules is None or "data" not in rules:
+        return None
+
+    ids = list(map(lambda rule: rule["id"], rules["data"]))
+    payload = {"delete": {"ids": ids}}
+    response = requests.post(
+        "https://api.twitter.com/2/tweets/search/stream/rules",
+        auth=bearer_oauth,
+        json=payload
+    )
+    if response.status_code != 200:
+        raise Exception(
+            "Cannot delete rules (HTTP {}): {}".format(
+                response.status_code, response.text
+            )
+        )
+    print('delete_all_rules(rules) response:')    
+    
+    (json.dumps(response.json()))
+
+
+def set_rules(rules):
+    # You can adjust the rules if needed 
+    # if the passed in rules is null, then 
+    if rules is None:
+        sample_rules = [
+            {"value": "dog has:images", "tag": "dog pictures"},
+            {"value": "cat has:images -grumpy", "tag": "cat pictures"},
+        ]
+    else:
+        print('assigning specified rules')
+        sample_rules = rules
+    payload = {"add": sample_rules}
+    response = requests.post(
+        "https://api.twitter.com/2/tweets/search/stream/rules",
+        auth=bearer_oauth,
+        json=payload,
+    )
+    if response.status_code != 201:
+        raise Exception(
+            "Cannot add rules (HTTP {}): {}".format(response.status_code, response.text)
+        )
+    print('set_rules(delete) response:')      
+    print(json.dumps(response.json()))
+
+
+def get_stream(countOfTweets):
+    countOfTweets = int(countOfTweets)
+    response = requests.get(
+        "https://api.twitter.com/2/tweets/search/stream", auth=bearer_oauth, stream=True,
+    )
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(
+            "Cannot get stream (HTTP {}): {}".format(
+                response.status_code, response.text
+            )
+        )
+    all_responses = []
+    count = 0
+    for response_line in response.iter_lines():
+        if count >= countOfTweets:
+            break;
+
+        if response_line:
+            # get the current tweet json
+            json_response = json.loads(response_line)    
+ 
+            # Keep count of the processed tweets
+            count = count + 1 
+
+            # initialize a dict to hold the current tweet
+            tweet = {}
+
+            # extract the data from the tweet and store it in our variable
+            tweet['count'] = count
+            tweet['id'] = json_response["data"]["id"]
+            tweet["text"] = json_response["data"]["text"]
+            tweet["tag"] = json_response["matching_rules"][0]["tag"]
+
+            # Update Session with current tweet.
+            session['current_tweet'] = tweet 
+
+            # add the tweet to our response list
+            all_responses.append(tweet)
+
+            #print(json.dumps(json_response, indent=4, sort_keys=True))
+            print('tweets streamed: ' + str(count))
+    #take the streamed responses, put it into a dataframe and print the dataframe        
+    #df = pd.DataFrame(all_responses)
+    #print(df)
+
+    #return the streamed responses         
+    return(all_responses)
+
+
+            
+
+def main():
+    rules = get_rules()
+    delete = delete_all_rules(rules)
+    set = set_rules(delete)
+    get_stream(set) 
+    
+
+
+#if __name__ == "__main__":
+    main()
